3a523269a0c242de1d2278480010a3fb05d88862
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2023-07-22 13:30:09 -0700

    all task

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,15 +6,18 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route("/", strict_slashes=False)
 def index():
     """displays Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """displays HBNB"""
     return 'HBNB'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
